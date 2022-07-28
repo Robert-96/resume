aa8855c3ea7e218e3bad34ed87553f4a9793f5ab
Robert Dezmerean <robert.dezmerean@altom.com>
2022-07-29 01:12:11 +0300

    Initial commit

---
+++ scripts/build.py
@@ -0,0 +1,27 @@
+import json
+
+from rost import Rost
+
+
+def get_context():
+    with open("data/data.json") as fp:
+        return json.load(fp)
+
+
+def build(debug=False):
+    generator = Rost(
+        searchpath="templates",
+        outputpath="dist",
+        contexts=[
+            ('.', get_context)
+        ],
+    )
+
+    if debug:
+        generator.watch(monitorpaths=["data/"])
+    else:
+        generator.build()
+
+
+if __name__ == "__main__":
+    build(debug=True)
