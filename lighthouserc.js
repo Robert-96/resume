03e5a19c38c88fd7bb5fb9c74ce754af2ee7c866
Robert-96 <dezmereanrobert@gmail.com>
2023-02-14 22:27:19 +0200

    Add new job for lighthouse ci

---
+++ lighthouserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './dist/',
+      maxAutodiscoverUrls: 1,
+      numberOfRuns: 5
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
