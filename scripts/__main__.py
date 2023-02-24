40bedbc5974237f080bdcb5a615a79f7ba5e03e1
Robert-96 <dezmereanrobert@gmail.com>
2023-02-24 21:12:42 +0200

    Add a --debug option to the build script

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,3 +1,3 @@
-from .build import build
+from .cli import cli
 
-build()
+cli()
---
+++ scripts/cli.py
@@ -0,0 +1,12 @@
+import argparse
+from pathlib import Path
+
+from .build import build
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    build(args.debug)
